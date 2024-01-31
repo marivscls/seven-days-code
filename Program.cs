1a5dbefca884e8c27c2c1ab926df1ad8f5b3f5a4
marivscls <vasconcelosmariana05@gmail.com>
2024-01-31 17:33:11 -0300

    first day

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+class Program
+{
+    static async Task Main()
+    {
+        var options = new RestClientOptions("https://pokeapi.co/api/v2/")
+        {
+            MaxTimeout = -1,
+        };
+
+        var client = new RestClient(options);
+        var request = new RestRequest("pokemon/1/", Method.Get);
+
+        RestResponse response = await client.ExecuteAsync(request);
+
+        if (response.IsSuccessful)
+        {
+            Console.WriteLine(response.Content);
+        }
+        else
+        {
+            Console.WriteLine($"Erro na requisição: {response.ErrorMessage}");
+        }
+    }
+}
